e05701677b6b42aaed5a3c5b4c7698db55203c61
Егор Рачев <rachjoves@std.tyuiu.ru>
2023-09-27 21:33:32 +0500

    Task3

---
+++ Tyuiu.RachevES.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.RachevES.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.RachevES.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c4a6faff-bda4-4d76-8929-c87709b6b57f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.RachevES.Sprint0.Task3.V0/Program.cs
+++ Tyuiu.RachevES.Sprint0.Task3.V0/Program.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.RachevES.Sprint0.Task2.V0.Lib;
+using Tyuiu.RachevES.Sprint0.Task3.V0.Lib;
 
-namespace Tyuiu.RachevES.Sprint0.Task2.V0
+namespace Tyuiu.RachevES.Sprint0.Task3.V0
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(DataService.GetMessage("Егор"));
+            Console.WriteLine(DataService.Sum(7, 7));
             Console.ReadKey();
+       
         }
     }
 }
