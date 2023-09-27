5b2218793b9adfbc24880b76f78c6abcb2c4615c
Егор Рачев <rachjoves@std.tyuiu.ru>
2023-09-27 16:35:38 +0500

    Task2

---
+++ Tyuiu.RachevES.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.RachevES.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.RachevES.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGitMessageValid()
+        {
+            var name = "Егор";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Егор", res);
+        }
+    }
+}
